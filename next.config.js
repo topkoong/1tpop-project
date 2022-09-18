24124d456865c27f91ca42709e2726446c5e3cba
toppy <theerut@shoplive.dev>
2022-09-18 19:20:58 +0700

    Added basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const dev = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: dev ? '' : '/1tpop-project',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
