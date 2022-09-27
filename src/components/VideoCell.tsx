9eb0982cfbb09ef55b8eb33a8de5bb1fb51ef037
toppy <theerut@shoplive.dev>
2022-09-28 00:16:57 +0700

    Fixed table header video styling

--- src/components/VideoCell.tsx
+++ src/components/VideoCell.tsx
@@ -9,6 +9,7 @@ const VideoCell: FunctionComponent<any> = ({ row }) => {
         <Link href={row.original.url} passHref>
           <a target='_blank' rel='noopener noreferrer'>
             <svg
+              className='hidden lg:block'
               width='36'
               height='31'
               viewBox='0 0 36 31'
@@ -18,6 +19,20 @@ const VideoCell: FunctionComponent<any> = ({ row }) => {
               <rect width='35.65' height='31' rx='13' fill='#FF0707' />
               <path
                 d='M25.575 15.8875L13.3688 22.9348L13.3688 8.84022L25.575 15.8875Z'
+                fill='white'
+              />
+            </svg>
+            <svg
+              className='block lg:hidden'
+              width='23'
+              height='19'
+              viewBox='0 0 23 19'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <rect width='22.1236' height='19' rx='7' fill='#FE4949' />
+              <path
+                d='M16.2239 9.49966L8.48062 13.9297L8.48062 5.06961L16.2239 9.49966Z'
                 fill='white'
               />
             </svg>
