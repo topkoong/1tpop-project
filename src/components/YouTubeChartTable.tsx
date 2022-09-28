3cd120c5f4cc76f9044d69bcc5d1af21e65fea6c
toppy <theerut@shoplive.dev>
2022-09-28 19:22:02 +0700

    Fixed safari table header issue

--- src/components/YouTubeChartTable.tsx
+++ src/components/YouTubeChartTable.tsx
@@ -7,7 +7,7 @@ const YoutubeChartTable: FunctionComponent<any> = ({
 }) => {
   return (
     <table className='w-full table-auto'>
-      <thead className='sticky top-0 bg-gradient-to-r from-[#FF2727] to-[#0047FF] h-[64px]'>
+      <thead className='bg-fixed sticky top-0 bg-gradient-to-r from-[#FF2727] to-[#0047FF] h-[64px]'>
         {getHeaderGroups().map((headerGroup: any) => (
           <tr key={headerGroup.id} className='border-b-2'>
             {headerGroup.headers.map((header: any) => (
