b87df9982c22a9062f0ac71da9f0df7cb8bce7f7
toppy <theerut@shoplive.dev>
2022-10-02 22:17:49 +0700

    Updated home page ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ import Spinner from '@components/Spinner';
 import axios from 'axios';
 import { isEmpty } from 'lodash';
 import moment from 'moment-timezone';
+import useBreakpoints from '@hooks/useBreakpoints';
 import { useQuery } from 'react-query';
 
 const fetchTopViewsVideosInfos = async (): Promise<any> => {
@@ -48,6 +49,7 @@ const Home: NextPage = () => {
     .tz('Asia/Bangkok')
     .format('l');
   const endOfWeek = moment().endOf('isoWeek').tz('Asia/Bangkok').format('l');
+  const { isLg, isXl, is2Xl } = useBreakpoints();
   const {
     isLoading,
     isError,
@@ -88,7 +90,7 @@ const Home: NextPage = () => {
         <meta property='og:site_name' content='1TPOP' />
         <link rel='icon' href='./favicon.ico' />
       </Head>
-      <main className='w-full h-full'>
+      <main className='w-full h-full bg-[#efefef]'>
         {isLoadingImages || isEmpty(imageSliders) ? (
           <Spinner />
         ) : isFetchingImagesSlidersError &&
@@ -125,8 +127,8 @@ const Home: NextPage = () => {
             <div className='flex-grow border-t h-[8px] md:h-[18px] bg-gradient-to-r from-[#FF2727] to-[#0047FF]'></div>
           </div>
         </section>
-        <section className='p-8 my-4 md:my-24 h-full bg-black'>
-          <div className='flex justify-between mb-8'>
+        <section className='p-8 my-4 md:my-0 h-full'>
+          <div className='p-8 flex flex-col md:flex-row justify-between bg-[#0047FF]'>
             <h1 className='text-base lg:text-3xl uppercase font-bold text-white'>
               YouTube’s T-POP Top Songs Chart This Week!{' '}
               <span className='inline-block text-sm md:text-base lg:text-xl xl:text-2xl font-bold text-white'>
@@ -144,38 +146,273 @@ const Home: NextPage = () => {
           ) : isError && error instanceof Error ? (
             <span>Error: {error?.message} </span>
           ) : (
-            <div className='flex gap-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-8 p-8 bg-[#3D3D3D]'>
               <div className='w-full'>
-                <p className='text-white text-base my-2'>
-                  {videoInfos[0].title}
-                </p>
                 <img
                   className='w-full'
                   src={videoInfos[0].maxresImageUrl}
                   alt=''
                 />
               </div>
-              <div className='w-full'>
-                {videoInfos.slice(1).map((videoInfo: any) => (
+              <div className='w-full grid grid-rows-3 gap-4'>
+                {videoInfos.map((videoInfo: any, idx: number) => (
                   <div
                     key={videoInfo.title}
-                    className='flex items-center gap-8 my-2'
+                    className='grid grid-cols-3 bg-white w-full'
                   >
-                    <img
-                      className='w-[240px] md:w-[280px] lg:w-[320px] xl:w-[360px] 2xl:w-[380px]'
-                      src={
-                        videoInfo.maxresImageUrl ??
-                        videoInfo.standardImageUrl ??
-                        videoInfo.highImageUrl ??
-                        videoInfo.mediumImageUrl
-                      }
-                      alt=''
-                    />
-                    <p className='text-white text-base'>{videoInfo.title}</p>
+                    <div className='bg-[#0047FF] w-14 col-end-1 grid items-center'>
+                      <p className='text-white text-center text-xl sm:text-3xl md:text-4xl lg:text-5xl'>
+                        {idx + 1}
+                      </p>
+                    </div>
+                    <div className='w-full p-8 col-span-full'>
+                      <div className='grid grid-rows-2 h-full gap-4'>
+                        <div className='w-full'>
+                          <p className='text-black font-bold text-base lg:text-2xl'>
+                            {videoInfo.title}
+                          </p>
+                        </div>
+                        <div>
+                          <div className='flex justify-start gap-8'>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {' '}
+                                {isLg || isXl || is2Xl
+                                  ? videoInfo?.views
+                                      ?.toString()
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                                  : Intl.NumberFormat('en', {
+                                      notation: 'compact',
+                                    }).format(videoInfo.views)}
+                              </p>
+                            </div>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {moment(videoInfo.publishedAt)
+                                  .tz('Asia/Bangkok')
+                                  .format('l')}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 ))}
               </div>
             </div>
+          )}
+        </section>
+        <section className='p-8 my-4 md:my-0 h-full grid grid-cols-1 md:grid-cols-2 gap-8 p-8'>
+          {isLoading || isEmpty(videoInfos) ? (
+            <Spinner />
+          ) : isError && error instanceof Error ? (
+            <span>Error: {error?.message} </span>
+          ) : (
+            <>
+              <div className='w-full'>
+                <div className='p-8 flex justify-between bg-[#3D3D3D]'>
+                  <h1 className='text-base lg:text-xl uppercase font-bold text-white'>
+                    Most viewed YouTube T-Pop video last week
+                  </h1>
+                  <Link href='/charts'>
+                    <p className='cursor-pointer uppercase text-white text-sm lg:text-base xl:text-lg font-bold'>
+                      view all &gt;&gt;
+                    </p>
+                  </Link>
+                </div>
+                <img
+                  className='w-full'
+                  src={videoInfos[0].maxresImageUrl}
+                  alt=''
+                />
+                <div className='w-full grid gap-4'>
+                  <div
+                    key={videoInfos[0].title}
+                    className='grid grid-cols-3 bg-white w-full'
+                  >
+                    <div className='w-full p-8 col-span-full'>
+                      <div className='grid grid-rows-2 h-full gap-4'>
+                        <div className='w-full'>
+                          <p className='text-black font-bold text-base lg:text-2xl'>
+                            {videoInfos[0].title}
+                          </p>
+                        </div>
+                        <div>
+                          <div className='flex justify-start gap-8'>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {' '}
+                                {isLg || isXl || is2Xl
+                                  ? videoInfos[0]?.views
+                                      ?.toString()
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                                  : Intl.NumberFormat('en', {
+                                      notation: 'compact',
+                                    }).format(videoInfos[0].views)}
+                              </p>
+                            </div>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {moment(videoInfos[0].publishedAt)
+                                  .tz('Asia/Bangkok')
+                                  .format('l')}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='w-full'>
+                <div className='p-8 flex justify-between bg-[#3D3D3D]'>
+                  <h1 className='text-base lg:text-xl uppercase font-bold text-white'>
+                    Most viewed YouTube T-Pop video this month
+                  </h1>
+                  <Link href='/charts'>
+                    <p className='cursor-pointer uppercase text-white text-sm lg:text-base xl:text-lg font-bold'>
+                      view all &gt;&gt;
+                    </p>
+                  </Link>
+                </div>
+                <img
+                  className='w-full'
+                  src={videoInfos[1].maxresImageUrl}
+                  alt=''
+                />
+                <div className='w-full grid gap-4'>
+                  <div
+                    key={videoInfos[1].title}
+                    className='grid grid-cols-3 bg-white w-full'
+                  >
+                    <div className='w-full p-8 col-span-full'>
+                      <div className='grid grid-rows-2 h-full gap-4'>
+                        <div className='w-full'>
+                          <p className='text-black font-bold text-base lg:text-2xl'>
+                            {videoInfos[1].title}
+                          </p>
+                        </div>
+                        <div>
+                          <div className='flex justify-start gap-8'>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {' '}
+                                {isLg || isXl || is2Xl
+                                  ? videoInfos[1]?.views
+                                      ?.toString()
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                                  : Intl.NumberFormat('en', {
+                                      notation: 'compact',
+                                    }).format(videoInfos[1].views)}
+                              </p>
+                            </div>
+                            <div className='flex gap-4 items-center'>
+                              <svg
+                                width='33'
+                                height='34'
+                                viewBox='0 0 33 34'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <ellipse
+                                  cx='16.2785'
+                                  cy='16.7718'
+                                  rx='16.2785'
+                                  ry='16.7718'
+                                  fill='#D9D9D9'
+                                />
+                              </svg>
+                              <p className='font-bold text-sm md:text-base lg:text-lg'>
+                                {moment(videoInfos[0].publishedAt)
+                                  .tz('Asia/Bangkok')
+                                  .format('l')}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
           )}
         </section>
         <footer className='bg-black h-[190px] w-full flex justify-center items-center'>
