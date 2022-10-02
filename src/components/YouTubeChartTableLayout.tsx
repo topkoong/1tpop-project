12321a40d3bd38e066bbc8cf850112c2f263cd9b
toppy <theerut@shoplive.dev>
2022-10-02 23:41:05 +0700

    hide column in mobile screen

--- src/components/YouTubeChartTableLayout.tsx
+++ src/components/YouTubeChartTableLayout.tsx
@@ -13,10 +13,37 @@ import SongCell from '@components/SongCell';
 import VideoCell from '@components/VideoCell';
 import ViewsCell from '@components/ViewsCell';
 import YouTubeChartTable from '@components/YouTubeChartTable';
+import useBreakpoints from '@hooks/useBreakpoints';
 
 const YoutubeChartTableLayout: FunctionComponent<any> = ({ tableData }) => {
   const columnHelper = createColumnHelper<any>();
+  const { isXs, isSm } = useBreakpoints();
 
+  const mobileColumns = useMemo(
+    () => [
+      columnHelper.display({
+        header: 'Rank',
+        id: 'index',
+        cell: RankCell,
+      }),
+      columnHelper.accessor('song', {
+        header: 'Song',
+        id: 'song',
+        cell: SongCell,
+      }),
+      columnHelper.accessor('views', {
+        header: 'Views',
+        id: 'views',
+        cell: ViewsCell,
+      }),
+      columnHelper.display({
+        header: () => <p className='text-center'>Graph</p>,
+        id: 'index',
+        cell: GraphCell,
+      }),
+    ],
+    []
+  );
   const columns = useMemo(
     () => [
       columnHelper.display({
@@ -65,7 +92,7 @@ const YoutubeChartTableLayout: FunctionComponent<any> = ({ tableData }) => {
 
   const tableInstance = useReactTable({
     data: tableData as any,
-    columns: columns as any,
+    columns: isXs || isSm ? (mobileColumns as any) : (columns as any),
     initialState: {
       sorting: sortees,
     },
