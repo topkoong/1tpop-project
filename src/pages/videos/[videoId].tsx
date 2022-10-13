27610dd98821e6e22ee83b3c97a7a9a259102ec6
toppy <theerut@shoplive.dev>
2022-10-13 19:39:13 +0700

    testing ssr

--- src/pages/videos/[videoId].tsx
+++ src/pages/videos/[videoId].tsx
@@ -137,18 +137,18 @@ export async function getServerSideProps(context: any) {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // the path has not been generated.
-export async function getStaticPaths() {
-  const videoIds = await fetchDailyVideosIds();
-  // Get the paths we want to pre-render based on posts
-  const paths = videoIds.map((videoId: string) => ({
-    params: { videoId },
-  }));
+// export async function getStaticPaths() {
+//   const videoIds = await fetchDailyVideosIds();
+//   // Get the paths we want to pre-render based on posts
+//   const paths = videoIds.map((videoId: string) => ({
+//     params: { videoId },
+//   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: blocking } will server-render pages
-  // on-demand if the path doesn't exist.
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: blocking } will server-render pages
+//   // on-demand if the path doesn't exist.
 
-  return { paths, fallback: true }; // SSR page and then cache
-}
+//   return { paths, fallback: true }; // SSR page and then cache
+// }
 
 export default VideoPage;
